80170b2d3ea27691d5e146564924bbcfc5b03705
sinem taşpınar <taspinarsinem@hotmail.com>
2024-03-18 13:31:18 +0300

    some changes

---
+++ P02_AccessModifiers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace P02_AccessModifiers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
